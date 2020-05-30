25335a255f8dff514a4858a9ff033285d3a4d8fc
lazesergiu <lazesergiu04@gmail.com>
2020-05-30 11:25:46 +0100

    Add The halfHour parameter and the implementation with 30 min free parking time.

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,9 +5,9 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+    public void calculateFare(Ticket ticket, boolean halfHour) {
+        if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
+            throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
         }
 
         /**Changed getHour in getTime,
@@ -15,14 +15,23 @@ public class FareCalculatorService {
          *
          * The duration is a double not an integer,
          * Time in milliseconds is divided by one hour
+         *
          */
         long inHour = ticket.getInTime().getTime();
-        long  outHour = ticket.getOutTime().getTime();
-        double duration =(double) (outHour - inHour) /(1000 * 3600);
+        long outHour = ticket.getOutTime().getTime();
+        double duration = (double) (outHour - inHour) / (1000 * 3600);
+
+        /**Set a boolean parameter that represents the 30 min free time
+         *
+         * If the duration is less than 30 min, the duration is 0
+         * else, 30 min is subtracted from the duration
+         */
+
+        if (halfHour){
+            duration = (duration - 0.5 < 0) ? 0 : (duration - 0.5);}
 
 
-
-        switch (ticket.getParkingSpot().getParkingType()){
+        switch (ticket.getParkingSpot().getParkingType()) {
             case CAR: {
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
                 break;
@@ -31,7 +40,8 @@ public class FareCalculatorService {
                 ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
+            default:
+                throw new IllegalArgumentException("Unknown Parking Type");
         }
     }
 }
