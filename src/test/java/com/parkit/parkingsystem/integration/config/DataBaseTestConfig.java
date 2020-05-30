a6b901528e96c042d138aef4a26c076a2391f426
lazesergiu <lazesergiu04@gmail.com>
2020-05-30 11:56:10 +0100

    Add the implementation for the DataBaseConfig to the DataBaseTestConfig

--- src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
+++ src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
@@ -4,17 +4,23 @@ import com.parkit.parkingsystem.config.DataBaseConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.sql.*;
+import java.util.Properties;
 
 public class DataBaseTestConfig extends DataBaseConfig {
 
     private static final Logger logger = LogManager.getLogger("DataBaseTestConfig");
+    private static final String dbConfigFilePath = "./DBConfig.properties";
+
 
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         logger.info("Create DB connection");
         Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/test","root","rootroot");
+        return DriverManager.getConnection(dataBaseProperties().getProperty("db.url"),
+                dataBaseProperties().getProperty("db.user"), dataBaseProperties().getProperty("db.password"));
     }
 
     public void closeConnection(Connection con){
@@ -48,5 +54,22 @@ public class DataBaseTestConfig extends DataBaseConfig {
                 logger.error("Error while closing result set",e);
             }
         }
+    }
+    private Properties dataBaseProperties() {
+        Properties prop = new Properties();
+        try (OutputStream output = new FileOutputStream(dbConfigFilePath)) {
+
+            // set the properties value
+            prop.setProperty("db.url",
+                    "jdbc:mysql://localhost:3306/prod?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+            prop.setProperty("db.user", "root");
+            prop.setProperty("db.password", "rootroot");
+
+            // save properties to project root folder
+            prop.store(output, null);
+        } catch (IOException io) {
+            logger.error("Failed to store data base properties", io);
+        }
+        return prop;
     }
 }
